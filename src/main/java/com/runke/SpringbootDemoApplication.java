ae7f37e894111fc6caa4ff6aa5c311ed4e452420
137010393@qq.com <jiayouxl2012>
2020-03-02 21:04:42 +0800

    添加demo

---
+++ src/main/java/com/runke/SpringbootDemoApplication.java
@@ -0,0 +1,21 @@
+package com.runke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String hello(){
+        return "hello";
+    }
+
+}
